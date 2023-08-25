e0dacacd9196b17c79d906b5b184c17469c81e19
Joseph Hefner <Hefnerjoseph87@gmail.com>
2023-08-25 14:50:40 -0700

    Update nfl_schedule.R

--- nfl_schedule.R
+++ nfl_schedule.R
@@ -7,4 +7,4 @@ sched <- load_schedules(seasons = 2022) |>
   filter(home_team == "DEN" | away_team == "DEN") |> 
   select(season, game_type, week, away_team, home_team, away_score, home_score, result)
 
-write_csv(sched, "schedule_test.csv")  
+write_csv(sched, "Data/schedule_test.csv")  
